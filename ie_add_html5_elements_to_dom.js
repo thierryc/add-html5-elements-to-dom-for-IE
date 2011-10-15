9df108c8a22eb445fadf91fb54bf3b6ac835ee70
Thierry Charbonnel <thierry@creationflux.com>
2011-10-15 11:10:48 +0200

    add css

--- ie_add_html5_elements_to_dom.js
+++ ie_add_html5_elements_to_dom.js
@@ -1,2 +1,14 @@
+/*!
+ * CSS AP HTML5 for IE 0.1.0
+ *
+ * Copyright 2011, Thierry Charbonnel
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * https://github.com/thierryc
+ *
+ * Depends:
+ *  ie_html5_elements_dom.css
+ *
+ */
+
 var e = ("abbr,article,aside,audio,canvas,datalist,details,figcaption,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video").split(',');
 for (var i = 0; i < e.length; i++) { document.createElement(e[i]); }
